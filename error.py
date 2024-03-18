077a059ce611f0083b37b51aaf4ce198c223a5eb
LauDuhalde <duhalde.ulloa.laura@gmail.com>
2024-03-18 12:52:01 -0300

    first commit

---
+++ error.py
@@ -0,0 +1,15 @@
+class DimensionError(Exception):
+    def __init__(self, mensaje, dimension=None, maximo=None):
+        self.mensaje = mensaje
+        self.dimension = dimension
+        self.maximo = maximo
+    def __str__(self):
+        if(self.dimension is None and self.maximo is None):
+            return super.__str__()
+        else:
+            mensaje_retorno = f"Mensaje: {self.mensaje}"
+            if self.dimension is not None:
+                mensaje_retorno += f", Dimension: {self.dimension}"
+            if self.maximo is not None:
+                mensaje_retorno += f", Maximo: {self.maximo}"
+            return mensaje_retorno 
---
+++ foto.py
@@ -0,0 +1,37 @@
+from error import DimensionError
+class Foto():
+    MAX = 2500
+
+    def __init__(self, ancho: int, alto: int, ruta: str) -> None:
+        self.__ancho = ancho
+        self.__alto = alto
+        ruta = ruta
+
+    @property
+    def ancho(self) -> int:
+        return self.__ancho
+
+    @ancho.setter
+    def ancho(self, ancho) -> None:
+        if ancho < 1 or ancho > self.MAX:
+            raise DimensionError(mensaje="El nuevo ancho está fuera de rango.",dimension=ancho,maximo=self.MAX)
+        else:
+            self.__ancho = ancho
+
+    @property
+    def alto(self) -> int:
+        return self.__alto
+
+    @alto.setter
+    def alto(self, alto) -> None:
+        if alto < 1 or alto > self.MAX:
+            #raise DimensionError(mensaje="El nuevo alto está fuera de rango.",dimension=alto)
+            raise DimensionError(mensaje="El nuevo alto está fuera de rango.",dimension=alto,maximo=self.MAX)
+        else:
+            self.__alto = alto
+            
+            
+if __name__ == "__main__":
+    f = Foto(200,200,"ruta")
+    #f.alto=0
+    #f.ancho = 2501
